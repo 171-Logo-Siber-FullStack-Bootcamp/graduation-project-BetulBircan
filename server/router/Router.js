a6badf83d9fda15931ffe2801e5d531f13711715
Betül Bircan <86554799+BetulBircan@users.noreply.github.com>
2022-03-30 14:18:58 +0300

    Update Router.js

--- server/router/Router.js
+++ server/router/Router.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const Books =require('../controller/books')    //Kitaplar
 const Categories = require('../controller/categories') //Kategoriler
-const Authors = require('../controller/authors')
-const NewBooks = require('../controller/newbooks')
+const Authors = require('../controller/authors') //yazarlar
+const NewBooks = require('../controller/newbooks') newbooks
 const router = express.Router()
 
 //Books
@@ -16,10 +16,12 @@ router.get('/getelastic', Books.getBookElastic)
 router.get('/categories', Categories.getCategories)
 router.get('/category', Categories.getCategory)
 
+//authors
 router.get('/authors', Authors.getAuthors)
 router.get('/author', Authors.getAuthor)
 router.get('/authorscategory', Authors.getAuthorsFromCategories)
 
+//newbooks
 router.get('/newbooks', NewBooks.getNewBooks)
 router.get('/newbook', NewBooks.getNewBook)
 
